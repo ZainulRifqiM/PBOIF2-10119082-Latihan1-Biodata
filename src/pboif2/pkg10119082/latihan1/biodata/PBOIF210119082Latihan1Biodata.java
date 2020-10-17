914ca3fd706b549daafb04b13c7d222f5225bc6f
Zainul Rifqi <Zainul Rifqi@LAPTOP-O1E2M14H>
2020-10-17 16:26:34 +0700

    PBOIF2-10119082-Latihan1-Biodata

---
+++ src/pboif2/pkg10119082/latihan1/biodata/PBOIF210119082Latihan1Biodata.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119082.latihan1.biodata;
+
+/**
+ *
+ * @author Zainul Rifqi
+ * NAMA     : Zainul Rifqi Muwaffaq
+ * KELAS    : IF2
+ * NIM      : 10119082
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * jenis-jenis tipe data bilangan bulat
+ */
+public class PBOIF210119082Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------------");
+        System.out.println("NAMA\t\t: Zainul Rifqi Muwaffaq");
+        System.out.println("ALAMAT\t\t: Jalan Pasir Kampung");
+        System.out.println("TEMPAT LAHIR\t: Cianjur");
+        System.out.println("TANGGAL LAHIR\t: 28 April 2001");
+        System.out.println("---------------------");
+        System.out.println("HOBI\t\t: Nonton & Bermain Game");
+        System.out.println("MAKES\t\t: Nasi Goreng");
+        System.out.println("MIKES\t\t: Susu");
+        
+    }
+    
+}
